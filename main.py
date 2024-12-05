3f2adc0c2888c3376ca071d865f6dbcfd266d048
mariecarlander <112630292+mariecarlander@users.noreply.github.com>
2024-12-05 19:50:23 +0100

    fixat

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from scipy.optimize import root_scalar
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
 # Define the system (numerator and denominator of the transfer function)
 num = [16, 8]
 den = [1, 16, 32, 32, 0]
+# Target value in radians (-45 degrees)
+target_value_final = -3*np.pi / 4
+
+# Define the function based on the given equation
+def func_final(x):
+    return (1/4) * np.arctan(2*x) - np.pi/2 - np.arctan(x/2) - 2*np.arctan(x/4) - target_value_final
 
 
 
@@ -38,7 +46,43 @@ plt.xlabel('Frequency [rad/s]')
 plt.ylabel('Phase [degrees]')
 plt.xlim(0.1, 10)
 plt.ylim(-200, 90)
+plt.plot(func_final(w))
+plt.plot(2.038324649571132, -135, 'ro')
+plt.plot(2.816950008351725, 0, 'bo')
 plt.grid(True, which='both',)
 plt.figure()
 
+
+f_interp = interp1d(phase, w)
+
+# Desired y-value
+desired_y = -135
+
+# Get the corresponding x-value(s)
+x_val = f_interp(desired_y)
+
+print(f"x-value for y={desired_y}: {x_val}")
+
 plt.show()
+
+
+# Target value in radians (-135 degrees)
+target_value_final = -3*np.pi / 4
+
+# Define the function based on the given equation
+def func_final(x):
+    return np.arctan(2*x) - np.pi/2 - np.arctan(x/2) - 2*np.arctan(x/4) - target_value_final
+
+# Solve using Brent's method within a reasonable bracket
+solution_final = root_scalar(func_final, bracket=[-10, 10], method='brentq')
+
+# Check if the solution was found
+if solution_final.converged:
+    print(f"Solution for x: {solution_final.root}")
+else:
+    print("No solution found.")
+    
+print(func_final(2.816950008351725))
+
+
+
